143dd1f41b04059e3da98cd663f26a638f7e7612
Shlyapniik <roma20053@gmail.com>
2026-04-06 11:48:16 +0300

    Input corrected

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
@@ -6,39 +7,29 @@ class Program
     public static void Main()
     {
         ShowMenu();
-        int userInput = int.Parse(Console.ReadLine());
+        int userInput = ReadInt("Your operation choise: ");
 
         while (userInput != 0)
         {
             Console.Clear();
             SelectOperation(userInput);
             ShowMenu();
-            userInput = int.Parse(Console.ReadLine());  
+            userInput = ReadInt("Your operation choise: ");
         }
     }
 
     public static void SelectOperation(int userInput)
     {
-        int index;
-        string task, newTask;
         switch (userInput)
         {
             case 1:
-                Console.Write("Write the new task, what you want to added: ");
-                task = Console.ReadLine();
-                AddTask(task);
+                AddTask();
                 break;
             case 2:
-                Console.Write("Write the index of the task, what you want to changed: ");
-                index = int.Parse(Console.ReadLine());
-                Console.Write("Write the new task");
-                newTask = Console.ReadLine();
-                EditTask(index, newTask);
+                EditTask();
                 break;
             case 3:
-                Console.Write("Write the index of the task, what you want to changed: ");
-                index = int.Parse(Console.ReadLine());
-                DeleteTask(index);
+                DeleteTask();
                 break;
             case 4:
                 Console.WriteLine("List of tasks:");
@@ -49,38 +40,83 @@ class Program
         }
     }
 
-    public static void AddTask(string task)
+    public static void AddTask()
     {
+        Console.Write("Write the new task, what you want to added: ");
+        string task = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(task))
+        {
+            Console.WriteLine("Cannot add empty task");
+            return;
+        }
+
         taskList.Add(task);
     }
 
-    public static void EditTask(int index, string newTask)
+    public static void EditTask()
     {
+        int index = ReadInt("Write the index of task: ");
+        
+        if (index < 0 || index >= taskList.Count)
+        {
+            Console.WriteLine("Wrong index");
+            return;
+        }
+
+        Console.Write("Write the new task: ");
+        string newTask = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(newTask))
+        {
+            Console.WriteLine("Cannot add empty task");
+            return;
+        }
+   
         taskList[index] = newTask;
     }
 
-    public static void DeleteTask(int index)
+    public static void DeleteTask()
     {
+        int index = ReadInt("Write the index of the task, what you want to changed: ");
+
         taskList.RemoveAt(index);
     }
 
     public static void ShowList()
     {
-        foreach (string task in taskList)
+        if (taskList.Count == 0)
         {
-            Console.WriteLine(task);
+            Console.WriteLine("Not tasks yet");
+            return;
+        }
+
+        for (int i = 0; i <= taskList.Count-1; i++)
+        {
+            Console.WriteLine($"{i}. {taskList[i]}");
         }
     }
 
     public static void ShowMenu()
-{
-    Console.WriteLine("\nHi, I'm console CRUD programm.\nChoose the option:\n" +
-        "1. Add new task.\n" +
-        "2. Edit task.\n" +
-        "3. Remove task.\n" +
-        "4. Show tasks list.\n"+
-        "0. Exit program.");
-}
+    {
+        Console.WriteLine("\nHi, I'm console CRUD programm.\nChoose the option:\n" +
+            "1. Add new task.\n" +
+            "2. Edit task.\n" +
+            "3. Remove task.\n" +
+            "4. Show tasks list.\n"+
+            "0. Exit program.\n");
+    }
+
+    public static int ReadInt(string message)
+    {
+        int number;
+        Console.Write(message);
+        while (!int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.Write("Invalid input. Try again");
+        }
+        return number;
+    }
 }
 
 
